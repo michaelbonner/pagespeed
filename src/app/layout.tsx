be29540c69498570cd9560ada42da4d9269db411
Michael Bonner <michaelwbonner@gmail.com>
2023-03-06 14:01:27 -0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body>
         <div className="max-w-7xl px-10 mx-auto">{children}</div>
       </body>
