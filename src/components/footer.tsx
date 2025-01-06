f0cdd92f6f61a45af51e68d9e221f48bba760166
Michael Bonner <michaelwbonner@gmail.com>
2025-01-05 20:59:24 -0700

    update copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <div className="py-4 px-8 mt-8 text-gray-600 bg-gray-100 rounded-lg">
-      Made by{" "}
+      ©2023-{new Date().getFullYear()}{" "}
       <Link
         className="underline underline-offset-4"
         href="https://bootpackdigital.com"
       >
         Bootpack Digital
       </Link>
+      . All rights reserved.
     </div>
   );
 };
