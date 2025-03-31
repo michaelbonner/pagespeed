eb686d9d3bfa255c0b3ba451a7b0d845f5954e3f
Michael Bonner <michaelwbonner@gmail.com>
2025-03-31 16:34:18 -0600

    allow pagespeed to run for 120s

--- src/app/api/get-pagespeed-results/route.ts
+++ src/app/api/get-pagespeed-results/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 const allowedDomains = ["bootpackdigital.com", "michaelbonner.dev"];
 
 export const config = {
-  maxDuration: 60,
+  maxDuration: 120,
 };
 
 export async function GET(request: NextRequest) {
