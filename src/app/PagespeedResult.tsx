8bd9f3440e301956160c11487fc924fc867fc2d1
Michael Bonner <michaelwbonner@gmail.com>
2025-03-31 16:04:40 -0600

    better loading

--- src/app/PagespeedResult.tsx
+++ src/app/PagespeedResult.tsx
@@ -1,8 +1,7 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
-import { Suspense } from "react";
 
 const getApiHost = () => {
   if (process.env.VERCEL_ENV === "production") {
@@ -42,10 +41,63 @@ export const PageSpeedResult = ({
   strategy: "mobile" | "desktop";
   url: string;
 }) => {
-  const { data } = useSuspenseQuery({
+  const { data, status } = useQuery({
     queryKey: ["pageSpeedData", url, strategy],
     queryFn: () => getPageSpeedData(url, strategy),
+    retry: false,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
   });
+
+  if (status === "pending") {
+    return (
+      <p className="text-sm text-gray-500 py-4 animate-pulse">
+        Loading results...
+      </p>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="border-l-4 border-yellow-400 bg-yellow-50 p-4">
+        <div className="flex">
+          <div className="shrink-0">
+            <svg
+              className="size-5 text-yellow-400"
+              aria-hidden="true"
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              height="200px"
+              width="200px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <div className="ml-3">
+            <p className="text-sm text-yellow-700">
+              There was an error loading this data via the api.{" "}
+              <a
+                className="font-medium text-yellow-700 underline hover:text-yellow-600"
+                href={pageSpeedLink}
+                rel="noreferrer"
+                target="_blank"
+              >
+                Use this link to view it on pagespeed.web.dev
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   const categories = data?.lighthouseResult?.categories || {
     performance: { score: 0 },
