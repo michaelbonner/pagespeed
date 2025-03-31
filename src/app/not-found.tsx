d21ac0aa731d3e75fd40a6389c06455c8593e09f
Michael Bonner <michaelwbonner@gmail.com>
2025-03-31 14:37:03 -0600

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <div>
+        <h1 className="text-4xl font-bold">404: Not Found</h1>
+        <p className="text-lg">Could not find requested page.</p>
+      </div>
+      <Link className="text-blue-500 underline" href="/">
+        Go back to the home page
+      </Link>
+    </div>
+  );
+}
