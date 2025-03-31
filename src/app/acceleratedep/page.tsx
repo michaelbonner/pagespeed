e89623c37e55db94edbec53b9b3459b865bc2caa
Michael Bonner <michaelwbonner@gmail.com>
2025-03-31 17:56:59 -0600

    rename component

--- src/app/acceleratedep/page.tsx
+++ src/app/acceleratedep/page.tsx
@@ -4,6 +4,8 @@ const baseUrl = "https://www.acceleratedep.com";
 const paths = ["/", "/services", "/about", "/careers", "/contact"];
 const urls = paths.map((path) => `${baseUrl}${path}`);
 
-export default function AcceleratedEPPageSpeed() {
-  return <PageContent title="AcceleratedEP Page Speed" urls={urls} />;
+export default function AcceleratedEquityPlansPageSpeed() {
+  return (
+    <PageContent title="Accelerated Equity Plans Page Speed" urls={urls} />
+  );
 }
