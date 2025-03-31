bb4e883bc7b194fab285b96a8460d308ecd03ef1
Michael Bonner <michaelwbonner@gmail.com>
2025-03-31 15:28:20 -0600

    Limit hostnames

--- src/app/api/get-pagespeed-results/route.ts
+++ src/app/api/get-pagespeed-results/route.ts
@@ -1,11 +1,19 @@
 import { getPageSpeedData } from "@/functions/getPagespeedData";
 import { NextRequest, NextResponse } from "next/server";
 
+const allowedDomains = ["bootpackdigital.com", "michaelbonner.dev"];
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
 
   const strategy = searchParams.get("strategy") as "mobile" | "desktop";
   const url = searchParams.get("url") as string;
+
+  const domain = new URL(url).hostname;
+
+  if (!allowedDomains.includes(domain)) {
+    return NextResponse.json({ error: "Invalid domain" }, { status: 400 });
+  }
 
   try {
     const results = await getPageSpeedData(url, strategy);
