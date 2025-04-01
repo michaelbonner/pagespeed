53221ce96b1aa8e233b6a779b7a422f17e4d8efa
Michael Bonner <michaelwbonner@gmail.com>
2025-03-31 18:32:45 -0600

    fix type error

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,8 +2,14 @@ import { PageContent } from "../components/PageContent";
 import { getSiteData, sites } from "../data/sites";
 import { notFound } from "next/navigation";
 
-export default function PageSpeed({ params }: { params: { slug: string } }) {
-  const siteData = getSiteData(params.slug as keyof typeof sites);
+export default async function PageSpeed({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const siteData = getSiteData(slug as keyof typeof sites);
 
   if (!siteData) {
     return notFound();
