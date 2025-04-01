0233e65de197d85f15ca80763b16d20e84023f6c
Michael Bonner <michaelwbonner@gmail.com>
2025-04-01 17:54:33 -0600

    Style up the header a tiny bit

--- src/app/components/PageContent.tsx
+++ src/app/components/PageContent.tsx
@@ -10,7 +10,8 @@ export const PageContent = ({
   return (
     <div>
       <h1 className="mt-4 text-2xl lg:text-5xl">
-        PageSpeed results for {title}
+        PageSpeed results for{" "}
+        <code className="p-3 rounded-md bg-gray-100">{title}</code>
       </h1>
       <div className="grid gap-16 py-8 mt-4">
         {urls.map((url) => {
