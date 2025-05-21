6b4732191674a3a1d0f69074853040a82ca67e39
Michael Bonner <mike@bootpackdigital.com>
2025-05-21 09:57:23 -0600

    update metadata for slug sites

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,26 @@
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
+import { notFound } from "next/navigation";
 import { PageContent } from "../components/PageContent";
 import { getSiteData, sites } from "../data/sites";
-import { notFound } from "next/navigation";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+
+  const siteData = getSiteData(slug as keyof typeof sites);
+
+  if (!siteData) {
+    return notFound();
+  }
+
+  return {
+    title: `${siteData.title} PageSpeed testing results`,
+    description: `We keep track of the PageSpeed results for ${siteData.title} to help us improve the site.`,
+  };
+}
 
 export default async function PageSpeed({
   params,
