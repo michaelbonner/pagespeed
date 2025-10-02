408a105fb074f836c5ad54631fbb581fc741634e
Michael Bonner <mike@bootpackdigital.com>
2025-10-02 09:48:23 -0600

    Remove neon

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,3 @@
-import { drizzle } from "drizzle-orm/neon-http";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 
-export const db =
-  process.env.IS_USING_NEON === "false"
-    ? drizzlePg(process.env.DATABASE_URL!)
-    : drizzle(process.env.DATABASE_URL!);
+export const db = drizzlePg(process.env.DATABASE_URL!);
