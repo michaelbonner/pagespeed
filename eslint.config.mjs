ac5825f7371e3bd27756317b480585c44ce83383
Michael Bonner <mike@bootpackdigital.com>
2025-10-26 21:52:16 -0600

    Prettier and eslint updates

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import storybook from "eslint-plugin-storybook";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...storybook.configs["flat/recommended"],
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
