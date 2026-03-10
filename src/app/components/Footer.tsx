62e9fe86f5a04e34f1e42faff2381fd7471621fb
Michael Bonner <michaelwbonner@gmail.com>
2026-03-09 20:33:54 -0600

    Remove sites list from footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,25 +1,10 @@
 import Link from "next/link";
-import { sites } from "../data/sites";
 
 export const Footer = () => {
   return (
     <div className="max-w-screen-2xl mx-auto prose prose-lg">
       <div className="text-gray-600">
-        <div>
-          <h2>Sites</h2>
-          <nav className="flex flex-wrap gap-x-4 gap-y-2">
-            {Object.entries(sites).map(([key, site]) => (
-              <Link
-                key={key}
-                className="underline underline-offset-4 text-sky-600"
-                href={`/${key}`}
-              >
-                {site.title}
-              </Link>
-            ))}
-          </nav>
-        </div>
-        <div className="prose-sm mt-6">
+        <div className="prose-sm">
           ©2023-{new Date().getFullYear()}{" "}
           <Link
             className="underline underline-offset-4"
